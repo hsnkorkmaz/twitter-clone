fcc897f2fd736f02af011867c86b439be4da7b07
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-10-18 16:38:59 +0200

    add twitter colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitterPrimary: {
+          base: "hsl(203, 89%, 53%)",
+          dark: "hsl(203, 89%, 53%)",
+          light: "hsl(203, 89%, 53%)"
+        },
+        twitterGray: {
+          dark: "#657786",
+          light: "#AAB8C2",
+          extraLight: "#E1E8ED",
+          lightest: "#F5F8FA",
+        },
+        twitterBlack: "#14171A"
+      },
+    },
   },
   variants: {
     extend: {},
